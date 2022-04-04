eed92de763896e02c6cfce3552eb3935bd1ef083
tmax818 <tylermaxwell661@gmail.com>
2022-04-04 12:27:33 -0700

    add leture material

---
+++ lectures/day_01/files/dict.py
@@ -0,0 +1,8 @@
+
+
+person_list = ["Tyler", 39, True, True]
+
+person_dict = {'name': 'Tyler', 'age': 39}
+address = {'street': "123 Main", 'city': 'Santa Clarita', 'state': 'CA', 'zip': 91355}
+
+person_dict['address'] = address
---
+++ lectures/day_01/files/functions.py
@@ -0,0 +1,40 @@
+## functions.py
+## demo
+
+def add(param1, param2):
+    print(param1 + param2)
+    # return param1 + param2
+
+add(3,4)
+
+def divide(numerator, denominator = 1):
+    return numerator/denominator
+
+print(divide(10))
+print(divide(denominator = 2, numerator = 10))
+
+
+
+# TODO function invocation
+
+# TODO function_name('argument')
+
+## TODO returning values
+
+
+
+# def number_of_military_branches():
+#     return 5
+# print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
+
+#3
+def number_of_books_on_hold():
+    return 5
+    return 10
+print(number_of_books_on_hold())
+
+#6
+def add(b,c):
+    print(b+c)
+print(add(1,2) + add(2,3))
+
