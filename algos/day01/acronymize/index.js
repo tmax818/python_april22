0c819a1a0e57179078cea4a1234ca875d8ea093d
tmax818 <tylermaxwell661@gmail.com>
2022-04-04 11:30:34 -0700

    update riot

--- algos/day01/acronymize/index.js
+++ algos/day01/acronymize/index.js
@@ -3,7 +3,12 @@
   Create a function that, given a string, returns the string’s acronym 
   (first letter of each word capitalized). 
   Do it with .split first if you need to, then try to do it without
+  R read/restate
+  I input
+  O output
+  T talk
 */
+
 
 const str1_arg = " there's no free lunch - gotta pay yer way. ";
 const expected1 = "TNFL-GPYW";
@@ -18,4 +23,23 @@ const expected2 = "LFNYISN";
  * @param {string} str A string to be turned into an acronym.
  * @returns {string} The given str converted into an acronym.
  */
-function acronymize(str_param) {}
+// const str1_arg = " there's no free lunch - gotta pay yer way. ";
+// const expected1 = "TNFL-GPYW";
+function acronymize(str_param) {
+  // create a new str variable
+  let newStr = ""
+  // split the str
+  strArray = str_param.split(" ")
+  for(var i = 0; i < strArray.length; i++){
+    console.log(strArray)
+    // console.log(strArray[i].charAt(0).toUpperCase())
+    newStr += strArray[i].charAt(0).toUpperCase()
+  }
+  // parse/iterate the input from .split
+  // only return the first letters
+  console.log(i)
+  return newStr
+}
+// acronymize(str1_arg)
+console.log(acronymize(str1_arg))
+// console.log(acronymize(str2_arg))
