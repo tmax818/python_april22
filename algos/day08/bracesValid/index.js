b40d57108e03f7ab6fd8fa9f1529e8672c86e063
tmax818 <tylermaxwell661@gmail.com>
2022-04-13 10:19:02 -0700

    sol day 8

--- algos/day08/bracesValid/index.js
+++ algos/day08/bracesValid/index.js
@@ -22,6 +22,39 @@ const expected3 = false;
  */
 function bracesValid(str) {}
 
-console.log(bracesValid(str1))
-console.log(bracesValid(str2))
-console.log(bracesValid(str3))
+// console.log(bracesValid(str1))
+// console.log(bracesValid(str2))
+// console.log(bracesValid(str3))
+
+
+/**
+ const str1 = "W(a{t}s[o(n{ c}o)m]e )h[e{r}e]!";
+ * - Time: O(n * m) where n = str.length and m = opens.length,
+ *    since opens.length is constant length of 3 -> O(3n) -> O(n) linear.
+ * - Space: O(n) linear.
+ * @param {string} str
+ * @returns {boolean}
+ */
+function bracesValid2(str = "") {
+    const stack = [];
+    const opens = "({[";
+    const closeToOpen = { ")": "(", "}": "{", "]": "[" };
+    
+    for (let i = 0; i < str.length; i++) {
+        console.log("stack=", stack, "i=", str[i])
+        if (opens.includes(str[i])) {
+            stack.push(str[i]);
+        } else if (str[i] in closeToOpen) {
+            if (closeToOpen[str[i]] === stack[stack.length - 1]) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+}
+
+console.log(bracesValid2(str1))
+console.log(bracesValid2(str2))
+console.log(bracesValid2(str3))
